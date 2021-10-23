5ab36cae7c49e23e4151373d61e87be60136483c
Huntress of the Fallen <82283389+huntressofthefallen@users.noreply.github.com>
2021-10-23 21:10:13 +0700

    Restricted

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Andika & Лилия' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Jean & Soph', responded: 'visually-hidden' });
+});
+router.post('/', function (req, res, next) {
+  if (req.body.password.toLowerCase() == 'anchovy' || req.body.password.toLowerCase() == 'anchovies') {
+    res.render('first', { title: 'Andika & Лилия' });
+  }
+  else {
+    res.render('index', { title: 'Jean & Soph', responded: '' });
+  }
 });
 
 module.exports = router;
